5e2c54d04e6ba3345ff5a43efb6963d5d412c4f5
jooseeruu <arjona2908@gmail.com>
2024-10-11 21:15:25 +0200

    1-5

---
+++ 1ª EVA/ActividadOperadores/ejercicios1-5.js
@@ -0,0 +1,54 @@
+// Ejercicio 1
+function CalcPago() {
+  const horas = document.getElementById("horas").value;
+  const salario = document.getElementById("salario").value;
+  const pago = horas * salario;
+  document.getElementById(
+    "resultado1"
+  ).innerText = `Te corresponden ${pago} euros`;
+}
+
+// Ejercicio 2
+function CalcIMC() {
+  const peso = parseFloat(document.getElementById("peso").value);
+  const altura = parseFloat(document.getElementById("altura").value);
+  const IMC = peso / (altura * altura);
+  document.getElementById(
+    "resultado2"
+  ).innerText = `Tienes un IMC de ${IMC.toFixed(2)}`;
+}
+
+// Ejercicio 3
+function CalcDivision() {
+  const numero1 = parseFloat(document.getElementById("numero1").value);
+  const numero2 = parseFloat(document.getElementById("numero2").value);
+  const cociente = Math.floor(numero1 / numero2);
+  const resto = numero1 % numero2;
+  document.getElementById(
+    "resultadoCociente"
+  ).innerText = `El cociente es ${cociente}`;
+  document.getElementById("resultadoResto").innerText = `El resto es ${resto}`;
+}
+
+// Ejercicio 4
+function CalcInv() {
+  const cantidad = parseFloat(document.getElementById("cantidad").value);
+  const interes = parseFloat(document.getElementById("interes").value / 100);
+  const anos = parseFloat(document.getElementById("anos").value);
+  const capital = cantidad * (1 + interes) ** anos;
+  document.getElementById(
+    "resultado4"
+  ).innerText = `El capital obtenido en la inversión es: ${capital.toFixed(
+    2
+  )} €`;
+}
+
+// Ejercicio 5
+function CalcPeso() {
+  const payasos = document.getElementById("payasos").value;
+  const munecas = document.getElementById("munecas").value;
+  const pesoTotal = payasos * 112 + munecas * 75;
+  document.getElementById(
+    "resultado5"
+  ).innerText = `El peso total del paquete es: ${pesoTotal} g`;
+}
