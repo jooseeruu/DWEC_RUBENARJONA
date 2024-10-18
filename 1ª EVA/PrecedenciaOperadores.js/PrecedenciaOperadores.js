040647c53ab28732d9a0d9c25a4b137dccfe32d2
jooseeruu <arjona2908@gmail.com>
2024-10-18 12:51:10 +0200

    update

--- 1ª EVA/PrecedenciaOperadores.js/PrecedenciaOperadores.js
+++ 1ª EVA/PrecedenciaOperadores.js/PrecedenciaOperadores.js
@@ -1,13 +1,9 @@
-//1.¿Cuál será la salida de los siguientes códigos?
 alert(null || 2 || undefined); // 2
 alert(alert(1) || 2 || alert(3)); // 1 seguido de 2
 alert(1 && null && 2); // null
 alert(alert(1) && alert(2)); // 1 seguido de undefined
 alert(null || (2 && 3) || 4); // 3
 
-//2.¿Cuáles de estos alert va a ejecutarse? ¿Y cuáles serán los resultados de las expresiones dentro de if(...)?
-if (-1 || 0) alert("primero"); // El alert se ejecutará, resultado: "primero"
-
-if (-1 && 0) alert("segundo"); // El alert NO se ejecutará
-
-if (null || (-1 && 1)) alert("tercero"); // El alert se ejecutará, resultado: "tercero"
+if (-1 || 0) alert("primero"); // El alert se ejecuta y el resultado es primero
+if (-1 && 0) alert("segundo"); // El alert no se ejecuta
+if (null || (-1 && 1)) alert("tercero"); // El alert se ejecuta y el resultado es tercero
